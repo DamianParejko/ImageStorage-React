392861b82378356e0539865471fe211d1270fd81
alenumerek <damian.parejko@gmail.com>
2023-10-27 13:07:43 +0200

    Add redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist'
+import imageReducer from './state';
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+const persistConfig = { key: 'root', storage, blacklist:[], version: 1}
+const persistedReducer = persistReducer(persistConfig, imageReducer)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk]
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+    </PersistGate>
+  </Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/state/index.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    images: []
+}
+
+export const imageSlice = createSlice({
+    name: "image",
+    initialState,
+    reducers: {
+      
+    }
+})
+
+export default imageSlice.reducer;
